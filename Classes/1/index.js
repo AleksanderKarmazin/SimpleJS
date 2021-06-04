e1e69d240bed9f98cec8b668631613e69025ab73
karmazin <kubartphil@gmail.com>
2021-06-04 12:36:55 +0300

    Context this, call, apply, bind. Part 1

--- Classes/1/index.js
+++ Classes/1/index.js
@@ -1,2 +1,72 @@
 'use strict';
 
+function getName() {
+    return this.name
+}
+
+let item = {
+    name:"Intel Cors i7",
+    type: "Processor",
+    price:200,
+    getPrice: function(){
+        console.log(this);
+        // Для этой фунции окружение (контекст) === item2
+        return this.price
+    },
+    getName: getName,
+}  
+
+let item2 = {
+    name:"Intel Cors i3",
+    type: "Processor",
+    price:100,
+    // Для этой фунции окружение (контекст) === item2
+    getPrice: function(){
+        console.log(this);
+        return item2.price
+    },
+    getName: getName,
+} 
+
+// Для этой фунции окружение (контекст) === window 
+// окно браузера
+function foo() {
+   
+}
+
+/* 
+Когда мы вызывали функцию внутри объекта 
+то контекстом для этой функции 
+будет объект в котором функцию создана 
+
+*/
+console.log(item.getPrice());
+console.log(item2.getPrice());
+
+
+let itemA = {
+    name:"Intel Cors i7",
+    type: "Processor",
+    price:200,
+    getPrice: ()=>{
+        console.log(this);
+        return itemA.price
+    },
+    getName: getName,
+} 
+
+let itemA2 = {
+    name:"Intel Cors i3",
+    type: "Processor",
+    price:100,
+    // Для этой фунции окружение (контекст) === item2
+    getPrice: ()=>{
+        console.log(this);
+        return itemA2.price
+    }
+} 
+
+
+console.log(itemA.getPrice());
+console.log(itemA2.getPrice());
+ 
